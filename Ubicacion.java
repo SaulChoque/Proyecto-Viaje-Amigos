076c62b10e1c3757e36e197b0969b39f43b82564
thravid <107086182+thravid@users.noreply.github.com>
2022-06-08 21:10:23 -0400

    Update Ubicacion.java
    completo hacer correr

--- Ubicacion.java
+++ Ubicacion.java
@@ -1,1 +1,47 @@
+public class Ubicacion {
+    
+    private String departamento;
+    private String zona;
 
+    /**
+     * @return the departamento
+     */
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    /**
+     * @param departamento the departamento to set
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    /**
+     * @return the zona
+     */
+    public String getZona() {
+        return zona;
+    }
+
+    /**
+     * @param zona the zona to set
+     */
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+    public  Ubicacion()
+    {
+    departamento="la paz";
+    zona="obrajes";
+    }
+    public  Ubicacion(String depa,String zon)
+    {
+        departamento=depa;
+        zona=zon;
+    }
+    public void mostrar()
+    {
+        System.out.println(" Departamento "+getDepartamento()+" Zona "+getZona());
+    }
+}
