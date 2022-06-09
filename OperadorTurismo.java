18116f0d89c96b7b167301d750322e0743f100dc
thravid <107086182+thravid@users.noreply.github.com>
2022-06-08 21:10:12 -0400

    Update OperadorTurismo.java
    hacer correr y revisar codigo

--- OperadorTurismo.java
+++ OperadorTurismo.java
@@ -1,1 +1,162 @@
 
+import proyectomenfy.Ubicacion;
+import proyectomenfy.paquete;
+
+public class OperadorTurismo {
+    
+    private String nombre;
+    private String descripcion;
+    private Ubicacion direccion;
+    private int telefono;
+    paquete paquetes[]=new paquete[20];
+    private int nroPaquetes;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the direccion
+     */
+    
+
+    /**
+     * @return the telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the nroPaquetes
+     */
+    public int getNroPaquetes() {
+        return nroPaquetes;
+    }
+    
+
+    /**
+     * @param nroPaquetes the nroPaquetes to set
+     */
+    public void setNroPaquetes(int nroPaquetes) {
+        this.nroPaquetes = nroPaquetes;
+    }
+    public  OperadorTurismo()
+    {
+       nombre="Sport Turing";
+       descripcion="dedicados a el vieje y deportes extremos en el oriente boliviano creando momentos inolvidables";
+       
+        direccion=new Ubicacion();
+        telefono=2830973;
+        for (int i = 1; i < 19+1; i++) {
+            paquetes[i]=new paquete();
+        }
+    
+        nroPaquetes=3;
+    }
+    public OperadorTurismo(String n,String des,Ubicacion ubi,int tele,int nro)
+    {
+        nombre=n;
+       descripcion=des;
+       direccion=ubi;
+        telefono=tele;
+        for (int i = 1; i < 19+1; i++) {
+            paquetes[i]=new paquete();
+        }
+        nroPaquetes=nro;
+    }
+    public void mostrar()
+    {
+        System.out.println(" nombre "+getNombre()+" descripcion "+getDescripcion()+" telefono "+getTelefono()+" numero de paquetes"+getNroPaquetes()+" direccion ");
+        direccion.mostrar();
+        for (int i = 1; i < getNroPaquetes()+1; i++) {
+            paquetes[i].mostrar();
+        }
+    }
+    public void mostrarResumen()
+    {
+        System.out.println(" nombre "+getNombre()+" descripcion "+getDescripcion());
+        for (int i = 1; i < getNroPaquetes()+1; i++) {
+            paquetes[i].mostrar();
+        }
+    }
+    public void destino(Ubicacion des)
+    {
+        for (int i = 1; i < getNroPaquetes()+1; i++) {
+            
+            if (paquetes[i].getDestino().equals(des))
+                {
+                      mostrarResumen();
+                }
+        }
+    }
+    public void mostPrecio(int x)
+    {
+        for (int i = 1; i < getNroPaquetes()+1; i++) {
+            
+            if (paquetes[i].getPrecio()==x)
+                {
+                      mostrarResumen();
+                }
+        }
+    }
+    public  void añadirUbicacion(Ubicacion ubi)
+    {
+        setDireccion(ubi);
+    }
+
+    /**
+     * @return the direccion
+     */
+    public Ubicacion getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(Ubicacion direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the ubicacion
+     */
+    public void leer()
+    {
+         for (int i = 1; i < getNroPaquetes()+1; i++) {
+            paquetes[i].leer();
+        }
+    }
+}
