a0cb3a55de2839b74b0a8044ee8f8548f709eadf
Jfernand3z <jcfernandez597@gmail.com>
2022-06-09 21:45:39 -0400

    first

--- Viaje.java
+++ Viaje.java
@@ -1,7 +1,34 @@
 public class Viaje {
-    private String algo;
-    private int gg;
-    private String HolaMundo;
-    // dVID
+    private String fechaInicio;
+    private String fechaFinal;
+    private int nroAmigos; 
+    private Amigo amigos[] = new Amigo[20];
+    private ArchivoOT operador;
+    private Paquete paquete;
+    private int precioTotal;
     
+    public void leerAmigos() 
+    {
+		
+	}
+    public void mostrarAmigos() 
+    {
+		
+	}
+    public void mostrar() 
+    {
+		
+	}
+    public void seleccionarOT() 
+    {
+		
+	}
+    public void lector() 
+    {
+		
+	}
+    public void escritor() 
+    {
+		
+	}
 }
