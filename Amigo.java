457190ce7e611b3941133ad00dbec91779a44ba0
Jfernand3z <jcfernandez597@gmail.com>
2022-06-10 15:39:24 -0400

    2do

---
+++ Amigo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Amigo extends Persona{
+
+	private int telefono;
+	
+	public void leer() 
+	{
+		Scanner lee = new Scanner(System.in);
+	}
+	public void mostrar() 
+	{
+		
+	}
+	public void lector() 
+	{
+		
+	}
+	public void escritor() 
+	{
+		
+	}
+}
---
+++ Persona.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Persona {
+
+	protected String nombre;
+	protected int ci;
+	protected char genero;
+	
+	public void leer() 
+	{
+		Scanner lee = new Scanner(System.in);
+		
+	}
+	public void mostrar() 
+	{
+		System.out.println("");
+	}
+	public void lector() 
+	{
+		
+	}
+	public void escritor() 
+	{
+		
+	}
+}
