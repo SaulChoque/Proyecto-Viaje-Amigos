49b78b0c6c4f8ad7db056076b27b2ecfe0c96776
[SaulChoque] <schoquehuancah@fcpn.edu.bo>
2022-06-11 17:22:05 -0400

    Delete Itinerario
    adios itinerario, te ira mejor en el infierno

--- Paquete.java
+++ Paquete.java
@@ -3,36 +3,27 @@ import java.io.Serializable;
 
 public class Paquete implements Serializable{
     private String codigo;
-    private int precio, nroItinerario;
+    private int precio;
     private Transporte tranIda, tranVuelta;
     private Hospedaje hosp;
     private Ubicacion destino;
-    private Itinerario[]itinerarioDia=new Itinerario[4];
     private ArrayList<String> incluye,noIncluye;
 
     public Paquete(){
         codigo="";
         precio=0;
-        nroItinerario=0;
         tranIda= new Transporte();
         tranVuelta= new Transporte();
         hosp= new Hospedaje();
-        for(int i=1; i<nroItinerario; i++){
-            itinerarioDia[i]=new Itinerario();
-        }
         destino=new Ubicacion(); 
     }
 
-    public Paquete(String a, int b, int c, Hospedaje f){
+    public Paquete(String a, int b, Hospedaje f){
         codigo=a;
         precio=b;
-        nroItinerario=c;
         tranIda=new Transporte();
         tranVuelta=new Transporte();
         hosp=f;
-        for(int i=0; i<nroItinerario; i++){
-            itinerarioDia[i]=new Itinerario();
-        }
         destino=new Ubicacion();
     }
 
@@ -40,7 +31,6 @@ public class Paquete implements Serializable{
         String a="";
         codigo=Leer.dato();
         precio=Leer.datoInt();
-        nroItinerario=Leer.datoInt();
         System.out.println(" Inserte no incluye: ");
         do{
             System.out.println(" => "); 
@@ -60,7 +50,7 @@ public class Paquete implements Serializable{
     }
 
     public void mostrar(){
-        System.out.println("Codigo: "+codigo+" || Precio: "+precio+" || nroItinerario: "+nroItinerario);
+        System.out.println("Codigo: "+codigo+" || Precio: "+precio);
         tranIda.mostrar();
         tranVuelta.mostrar();
         hosp.mostrar();
@@ -73,9 +63,6 @@ public class Paquete implements Serializable{
         }
     }
 
-    public void addIti(String a , String b, int c){
-        this.itinerarioDia[c].addItinerario(a, b);
-    }
 
     public void mostrarResumen(){
         System.out.println("Paquete a "+destino.getZona()+" por Bs. "+this.precio);
@@ -98,8 +85,6 @@ public class Paquete implements Serializable{
     public Hospedaje getHosp() {return hosp;}
     public ArrayList<String> getIncluye() {return incluye;}
     public ArrayList<String> getNoIncluye() {return noIncluye;}
-    public Itinerario[] getItinerarioDia() {return itinerarioDia;}
-    public int getNroItinerario() {return nroItinerario;}
     public int getPrecio() {return precio;}
     public Transporte getTranIda() {return tranIda;}
     public Transporte getTranVuelta() {return tranVuelta;}
@@ -108,9 +93,7 @@ public class Paquete implements Serializable{
     public void setCodigo(String codigo) {this.codigo = codigo;}
     public void setHosp(Hospedaje hosp) {this.hosp = hosp;}
     public void setIncluye(ArrayList<String> incluye) {this.incluye = incluye;}
-    public void setItinerarioDia(Itinerario[] itinerarioDia) {this.itinerarioDia = itinerarioDia;}
     public void setNoIncluye(ArrayList<String> noIncluye) {this.noIncluye = noIncluye;}
-    public void setNroItinerario(int nroItinerario) {this.nroItinerario = nroItinerario;}
     public void setTranIda(Transporte tranIda) {this.tranIda = tranIda;}
     public void setTranVuelta(Transporte tranVuelta) {this.tranVuelta = tranVuelta;}
     public void setDestino(Ubicacion destino) {this.destino = destino;}
