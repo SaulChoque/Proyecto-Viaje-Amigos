9586bad42c5f9964e5293f0e18184b0b691cf5b0
Jfernand3z <jcfernandez597@gmail.com>
2022-06-11 15:18:42 -0400

    ss

--- Principal.java
+++ Principal.java
@@ -4,19 +4,20 @@ public class Principal {
 	public static void main(String[] args) 
 	{
 		//JOSE MENU
-		bool sw = true;
-		while( sw ) {
-			System.out.println("\n ");
+		int op=0;
+		try {
+			do		
+			{System.out.println("\n ");
 			System.out.println(" MENU DE VIAJES ");
 			System.out.println(" ==== ");
 			System.out.println(" ");
 			System.out.println("1. CREAR VIAJE ");
 			System.out.println("2. MOSTRAR PAQUETE ");
 			System.out.println("3. SELECCIONAR PAQUETE ");
-			
-			
+		
+			op = Leer.datoInt();
 			//SAUL SWITCH
-			switch( Console.ReadKey().KeyChar ) {
+			switch(op) {
 
 				case '1':
 					Console.WriteLine();
@@ -48,7 +49,11 @@ public class Principal {
 					sw = false;
 					break;
 			}
+			}while(op>4)
+			}catch (Exception e) {
+			System.out.println("error"+e);
 		}
+		
 	}
 
 }
